011c062040191f7b3e1b2a0661110631b442f936
Yusuf Budak <yusufbdk34@gmail.com>
2022-09-29 20:25:43 +0200

    day9_javafaker_testbase_driverUtil

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -0,0 +1,38 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+
+public class JavaFakerPractice {
+
+    @Test
+    public void test1() {
+        //Creating Faker object to reach methods
+        Faker faker = new Faker();
+        //Faker faker = new Faker(new Locale("tr"));
+
+        System.out.println("faker.name().firstName() = " + faker.name().firstName());
+        System.out.println("faker.name().lastName() = " + faker.name().lastName());
+
+        System.out.println("faker.name().fullName() = " + faker.name().fullName());
+
+        //numerify() method will generate random numbers in the format we want to get
+        System.out.println("faker.numerify(\"###-###-####\") = " + faker.numerify("###-###-####"));
+
+        System.out.println("faker.numerify(\"076-###-####\") = " + faker.numerify("076-###-####"));
+
+        //letterify() method will return random letters where we pass "?"
+        System.out.println("faker.letterify(\"???-???\") = " + faker.letterify("???-???"));
+
+        //bothify() method will return random letters "?" and numbers "#"
+        System.out.println("faker.bothify(\"?#?#-##-??\") = " + faker.bothify("?#?#-##-??"));
+
+        System.out.println("faker.chuckNorris().fact() = " + faker.chuckNorris().fact().replaceAll("Chuck Norris", "Muhtar"));
+
+        System.out.println("faker.finance().creditCard() = " + faker.finance().creditCard().replaceAll("-", ""));
+
+
+    }
+
+
+}
