63f5292649305689363b516575c84192aef4a628
Yusuf Budak <yusufbdk34@gmail.com>
2022-09-29 20:27:05 +0200

    day7_webtables_utilities_javafaker

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,24 +1,14 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
-import com.cydeo.utilities.*;
+import com.cydeo.tests.base.TestBase;
 import com.cydeo.utilities.BrowserUtils;
-import com.cydeo.utilities.WebDriverFactory;
-import org.openqa.selenium.*;
-import org.testng.annotations.*;
+import com.cydeo.utilities.CRM_Utilities;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
-import java.util.concurrent.*;
+public class T3_CRM_LOGIN extends TestBase {
 
-public class T3_CRM_LOGIN {
-
-    public WebDriver driver;
-
-    @BeforeMethod
-    public void setupMethod(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-    }
 
     //TC #3: Login scenario
     @Test
